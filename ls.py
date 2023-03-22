1b776a80b4f040d4b766d1be14d8c2e3bdbdd703
elmathe022 <elmathe022@student.wethinkcode.co.za>
2023-03-22 16:57:01 +0200

    selected loadshedding areas to get,need to connect those to telegram bot

--- ls.py
+++ ls.py
@@ -1,33 +1,69 @@
 import requests
 import http.client
-from pprint import pprint
+import json
 conn = http.client.HTTPSConnection("developer.sepush.co.za")
+
 payload = ''
 headers = {"token" : "55015410-617D4AFD-8FFBA538-428E48F9"}
-conn.request("GET", "/business/2.0/area?id=ethekwini2-14-glenwood&test=current", payload, headers)
+id = "ethekwini2-14-glenwood"
+conn.request("GET", f"/business/2.0/area?id={id}", payload, headers)
 res = conn.getresponse()
 data = res.read()
-print(data.decode("utf-8"))
 
-import requests
+lis = data.decode("utf-8")
+datas = json.loads(lis)
 
-url = "https://developer.sepush.co.za/business/2.0/areas_nearby?lat=-29.8708&lon=30.9905"
+with open("load.json","w") as file:
+    json.dump(datas,file,indent=4)
 
-# payload={}
-# headers = {}
+print(type(datas))
+stage = 0 
+for date_ in datas["schedule"]["days"][0]["stages"]:
+    stage += 1
+    times = ""
+    if len(date_) == 0:
+        times = "No affected"
+    elif len(date_) == 1:
+        times += date_[0]
+    else:
+        for i in date_:
+            times += f"{i} "
+    print(f"stage: {stage} ,times: {times}")
 
-response = requests.request("GET", url, headers=headers, data=payload)
 
-print(response.text)
+area_ids = {
+    "glenwood 14": "ethekwini2-14-glenwood",
+    "umbilo" : "ethekwini2-23-umbilo",
+    "umbilo east 13" : "ethekwini2-13-umbiloeast",
+}
 
-import requests
+# for key,value in area_ids.items():
+#     print(key,value)
+#     conn.request("GET", f"/business/2.0/area?id={value}&test=current", payload, headers)
+#     res = conn.getresponse()
+#     data = res.read()
+#     print(data.decode("utf-8"))
+    
 
-url = "https://developer.sepush.co.za/business/2.0/status"
+# import requests
 
-# payload={}
-# headers = {}
+# url = "https://developer.sepush.co.za/business/2.0/areas_nearby?lat=-29.8842&lon=30.9790"
 
-response = requests.request("GET", url, headers=headers, data=payload)
+# # payload={}
+# # headers = {}
 
-print(response.text)
+# response = requests.request("GET", url, headers=headers, data=payload)
+
+# print(response.text)
+
+# import requests
+
+# url = "https://developer.sepush.co.za/business/2.0/status"
+
+# # payload={}
+# # headers = {}
+
+# response = requests.request("GET", url, headers=headers, data=payload)
+
+# print(response.text)
 
